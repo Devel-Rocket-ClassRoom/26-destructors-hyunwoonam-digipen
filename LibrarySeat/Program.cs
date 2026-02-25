5f3aaa1e4b284cc6ef702882bf62bcb4021ca2bf
hyunwoo.nam <hyunwoo.nam@digipen.edu>
2026-02-25 18:02:10 +0900

    작성중

--- LibrarySeat/Program.cs
+++ LibrarySeat/Program.cs
@@ -1,4 +1,22 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+Seat s1 = new Seat("김민수");
+Seat s2 = new Seat("이지영");
+Seat s3 = new Seat("박서준");
+
+s1.Study();
+s2.Study();
+s3.Study();
+
+Seat.ShowStatus();
+
+s1 = null;
+s2 = null;
+s3 = null;
+
+GC.Collect();
+GC.WaitForPendingFinalizers();
+
+Seat.ShowStatus();
---
+++ LibrarySeat/Seat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Seat
+{
+    private readonly int _id;
+
+    private string _name;
+
+    private static int count = 0;
+    private static int now = 0;
+
+    private static int _nextId = 1;
+
+    public Seat(string name)
+    {
+        _id = _nextId++;
+        _name = name;
+        count++;
+        now++;
+
+        Console.WriteLine($"좌석 {_id}번 착석: {_name}");
+    }
+
+    ~Seat()
+    {
+        now--;
+        Console.WriteLine($"좌석 {_id}번 반납: {_name}");
+    }
+
+    public void Study()
+    {
+        Console.WriteLine($"{_name}이(가) 좌석 {_id}번에서 공부 중...");
+    }
+    public static void ShowStatus()
+    {
+        Console.WriteLine($"총 이용: {count}명, 현재 착석: {now}명");
+    }
+
+}
+
